cf1e0574a78753c7d97c2eb8380078ed00ddaca7
andreichisilev <andreichisilev@gmail.com>
2023-05-12 20:06:57 +0300

    page structure for mobile

--- resources/js/script.js
+++ resources/js/script.js
@@ -1,5 +1,6 @@
 const navButton = document.getElementById('nav-toggle');
-const navMenu = document.querySelector('ul');
+const navMenu = document.querySelector('div.options ul');
+console.log(navMenu);
 
 navButton.addEventListener('click', () => {
     navMenu.classList.toggle('show');
