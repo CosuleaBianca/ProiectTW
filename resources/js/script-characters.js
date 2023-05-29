e0be2638b3d4a6248a4bce32698b447973864d45
andreichisilev <andreichisilev@gmail.com>
2023-05-30 00:51:35 +0300

    characters page update

---
+++ resources/js/script-characters.js
@@ -0,0 +1,32 @@
+const character = {
+    portraitURL:"",
+    name:"",
+    quote:"",
+    description:"",
+    personalDetails:{
+        fullName:"",
+        dateOfBirth:"",
+        alive:"",
+        dateOfDeath:"",
+        bloodStatus:"",
+        maritalStatus:"",
+        titles:[
+            
+        ],
+        species:"",
+        gender:"",
+        family:[
+
+        ],
+        occupation:[
+            
+        ],
+        house:""
+
+    }
+
+}
+
+function characterLoad() {
+    document.getElementsByTagName('title').in
+}
